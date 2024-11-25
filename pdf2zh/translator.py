6eed2ca09077816f60435bed2aaddaae441f99e7
hellofinch <543933756@qq.com>
2024-11-25 01:46:09 +0000

    修改deeplx模块拼接字符串错误

--- pdf2zh/translator.py
+++ pdf2zh/translator.py
@@ -168,6 +168,11 @@ class DeepLXTranslator(BaseTranslator):
             ) from e
 
         self.session = requests.Session()
+        server_url=server_url.rstrip('/')
+        if auth_key:
+            self.base_link = f"{server_url}/{auth_key}/translate"
+        else:
+            self.base_link = f"{server_url}/translate"
         self.base_link = f"{server_url}/{auth_key}/translate"
         self.headers = {
             "User-Agent": "Mozilla/4.0 (compatible;MSIE 6.0;Windows NT 5.1;SV1;.NET CLR 1.1.4322;.NET CLR 2.0.50727;.NET CLR 3.0.04506.30)"  # noqa: E501
