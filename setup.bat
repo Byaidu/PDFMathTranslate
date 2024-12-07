c886982bcc077b9dffcf4bd317d122a91c459765
Byaidu <909756245@qq.com>
2024-12-07 12:23:43 +0800

    fix: bat

--- setup.bat
+++ setup.bat
@@ -5,13 +5,13 @@ set PYTHON_URL=https://www.python.org/ftp/python/3.12.7/python-3.12.7-embed-amd6
 set PIP_URL=https://bootstrap.pypa.io/get-pip.py
 set HF_ENDPOINT=https://hf-mirror.com
 
-if not exist pdf2zh/python.exe (
+if not exist pdf2zh_dist/python.exe (
     powershell -Command "& {Invoke-WebRequest -Uri !PYTHON_URL! -OutFile python.zip}"
-    powershell -Command "& {Expand-Archive -Path python.zip -DestinationPath pdf2zh -Force}"
+    powershell -Command "& {Expand-Archive -Path python.zip -DestinationPath pdf2zh_dist -Force}"
     del python.zip
-    echo import site >> pdf2zh/python312._pth
+    echo import site >> pdf2zh_dist/python312._pth
 )
-cd pdf2zh
+cd pdf2zh_dist
 
 if not exist Scripts/pip.exe (
     powershell -Command "& {Invoke-WebRequest -Uri !PIP_URL! -OutFile get-pip.py}"
