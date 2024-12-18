dd6375bfd9017f21d33cf07ba59e9bd462b99d19
awwaawwa <8493196+awwaawwa@users.noreply.github.com>
2024-12-18 20:04:14 +0800

    translate函数中更好的处理envs和prompt不存在的情况

--- pdf2zh/high_level.py
+++ pdf2zh/high_level.py
@@ -297,7 +297,7 @@ def translate(
         doc_raw = open(file, "rb")
         s_raw = doc_raw.read()
         s_mono, s_dual = translate_stream(
-            s_raw, envs=kwarg.get("envs"), prompt=kwarg["prompt"], **locals()
+            s_raw, envs=kwarg.get("envs", {}), prompt=kwarg.get("prompt", []), **locals()
         )
         file_mono = Path(output) / f"{filename}-mono.pdf"
         file_dual = Path(output) / f"{filename}-dual.pdf"
