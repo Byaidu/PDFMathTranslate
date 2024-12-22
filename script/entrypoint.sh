c85d82a41e4bfb5388e64025680516519634a0e1
Tao Chen <iamtaochen@outlook.com>
2024-12-22 18:23:28 +0800

    mv auth logic to the last

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -54,14 +54,6 @@ fi
 # Clean up sensitive environment variables
 unset PDF2ZH_AUTH_USER_FILE PDF2ZH_AUTH_PASS_FILE _user_lines _pass_lines
 
-# Add authorization arguments if auth is enabled
-if [ "$_auth_enabled" = true ]; then
-    chmod 600 "$_auth_user_file" "$_auth_pass_file" || { echo "Error: Failed to set permissions on auth files"; exit 1; }
-    echo -e "\033[1;33mWarning: --authorized flag doesn't work, so disable auth.\033[0m"
-    # _tmp_args="${_tmp_args} --authorized ${_auth_user_file} ${_auth_pass_file}"
-fi
-
-
 # handle debug mode
 if [ "$PDF2ZH_DEBUG" = "true" ]; then
     _tmp_args="${_tmp_args} --debug"
@@ -89,11 +81,28 @@ start_time=$(date "+%Y-%m-%d %H:%M:%S")
 
 # Trim whitespace from _tmp_args
 _tmp_args=$(echo "${_tmp_args}" | xargs)
-# Define the command
+# Define the command which will be excuted
 cmd="pdf2zh --interactive ${_tmp_args} $PDF2ZH_OTHER_ARGS $@"
+cmd=$(echo $cmd | xargs)
+# Define the command to be printed, this is used to hide sensitive information
+_cmd_print=$cmd
+
+# Add authorization arguments if auth is enabled
+if [ "$_auth_enabled" = true ]; then
+    chmod 600 "$_auth_user_file" "$_auth_pass_file" || { echo "Error: Failed to set permissions on auth files"; exit 1; }
+    echo -e "\033[1;33mWarning: --authorized flag doesn't work, so disable auth. And Ignore the --authorized in the print message\033[0m"
+    # cmd="${cmd} --authorized ${_auth_user_file} ${_auth_pass_file}"
+    # Hide sensitive paths, only show filenames
+    _auth_user_file_name=$(basename "$_auth_user_file")
+    _auth_pass_file_name=$(basename "$_auth_pass_file")
+    # Update the printed command to hide full paths
+    _cmd_print="${_cmd_print} --authorized ****/${_auth_user_file_name} ****/${_auth_pass_file_name}"
+fi
+# Unset sensitive variables
+unset _auth_user_file _auth_pass_file
 
 # Print the command to be executed with the start time
-echo "[$start_time] Running: $cmd"
+echo "[$start_time] Running: $_cmd_print"
 echo "=================================================="
 
 # Execute the command
