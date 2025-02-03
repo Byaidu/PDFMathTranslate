9e7e9aacd16a69d317077f871695e8077f229204
awwaawwa <8493196+awwaawwa@users.noreply.github.com>
2025-02-04 00:27:26 +0800

    feat(pdf2zh): Support inserting fonts into indirectly referenced font resources
    - introduce re module for regex operations
    - extract and process font resource xref using regex
    - handle null font entries by setting appropriate keys

--- pdf2zh/high_level.py
+++ pdf2zh/high_level.py
@@ -3,6 +3,7 @@
 import asyncio
 import io
 import os
+import re
 import sys
 import tempfile
 import urllib.request
@@ -198,6 +199,20 @@ def translate_stream(
         for label in ["Resources/", ""]:  # 可能是基于 xobj 的 res
             try:  # xref 读写可能出错
                 font_res = doc_zh.xref_get_key(xref, f"{label}Font")
+                if font_res[0] == 'xref':
+                    resource_xref_id = re.search("(\\d+) 0 R", font_res[1]).group(1)
+                    xref = int(resource_xref_id)
+                    font_res = doc_zh.xref_object(xref)
+                    for font in font_list:
+                        font_exist = doc_zh.xref_get_key(
+                            xref, f"{font[0]}"
+                        )
+                        if font_exist[0] == "null":
+                            doc_zh.xref_set_key(
+                                xref,
+                                f"{font[0]}",
+                                f"{font_id[font[0]]} 0 R",
+                            )
                 if font_res[0] == "dict":
                     for font in font_list:
                         font_exist = doc_zh.xref_get_key(xref, f"{label}Font/{font[0]}")
